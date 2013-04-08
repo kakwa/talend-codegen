e60a66581ce3e41bcb42f43769e83a14648803cb
Craig Jones <Craig.Jones@utas.edu.au>
2013-04-09 00:38:58 +1000

    Read project file to get technical label for project rather than using the directory name as Jenkins doesn't keep the directory name

--- src/au/org/emii/talend/codegen/ProjectUtils.java
+++ src/au/org/emii/talend/codegen/ProjectUtils.java
@@ -1,13 +1,19 @@
 package au.org.emii.talend.codegen;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.ui.dialogs.IOverwriteQuery;
 import org.eclipse.ui.wizards.datatransfer.IImportStructureProvider;
 import org.eclipse.ui.wizards.datatransfer.ImportOperation;
@@ -15,7 +21,10 @@ import org.talend.commons.exception.BusinessException;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.commons.utils.workbench.resources.ResourceUtils;
 import org.talend.core.model.general.Project;
+import org.talend.core.model.properties.PropertiesPackage;
+import org.talend.core.repository.constants.FileConstants;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.repository.utils.URIHelper;
 import org.talend.core.repository.utils.XmiResourceManager;
 import org.talend.repository.ui.actions.importproject.FilterFileSystemStructureProvider;
 import org.talend.repository.ui.actions.importproject.ImportProjectsUtilities;
@@ -26,10 +35,10 @@ public class ProjectUtils {
     // Import project (refer org.talend.repository.ui.actions.importproject.ImportProjectsUtilities.importProjectAs
     // which is the TOS project import, but includes changing the name which we don't want here).
     	
-    public static Project importProject(String projectDir) throws InvocationTargetException, InterruptedException, PersistenceException, CoreException, BusinessException {
+    public static Project importProject(String projectDir) throws InvocationTargetException, InterruptedException, PersistenceException, CoreException, BusinessException, IOException {
         File directory = new File(projectDir);
         
-        String technicalName = directory.getName();
+        String technicalName = getTechnicalName(directory);
 
         // If project exists in repository already, delete it
        	deleteIfExists(technicalName);
@@ -53,6 +62,14 @@ public class ProjectUtils {
         
         return getTalendProject(technicalName);
     }
+
+	private static String getTechnicalName(File projectDir) throws IOException {
+        XmiResourceManager xrm = new XmiResourceManager();
+        Resource resource = xrm.resourceSet.getResource( URI.createFileURI(projectDir.getAbsolutePath() + File.separator + FileConstants.LOCAL_PROJECT_FILENAME), true);
+        org.talend.core.model.properties.Project emfProject = (org.talend.core.model.properties.Project) EcoreUtil
+                .getObjectByType(resource.getContents(), PropertiesPackage.eINSTANCE.getProject());
+		return emfProject.getTechnicalLabel();
+	}
 
 	// Delete specified project from the repository if it exists 
 	private static void deleteIfExists(String technicalName) throws CoreException {
